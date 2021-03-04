cefb4748114153ebfcbcfab7bcd811abb847a226
QuynhTrang2508 <uetquynhtrang2508@gmail.com>
2021-03-04 13:33:11 +0700

    task#34048

---
+++ app/src/main/java/com/example/yummy/ui/search/SearchFragment.kt
@@ -0,0 +1,17 @@
+package com.example.yummy.ui.search
+
+import com.example.yummy.R
+import com.example.yummy.base.BaseFragment
+
+class SearchFragment : BaseFragment() {
+    override val layoutResource get() = R.layout.fragment_search
+
+    override fun setupViews() {
+    }
+
+    override fun initData() {
+    }
+
+    override fun initActions() {
+    }
+}
